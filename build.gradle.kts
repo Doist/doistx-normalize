8a076fd66b69bbad7bb9ed876bb599f0ce9c4137
Gonçalo Silva <goncalossilva@gmail.com>
2021-05-15 04:19:39 +0100

    Compile before uploading
    Helps uploads go out simultaneously, which helps prevent errors.
    See: https://getstream.io/blog/publishing-libraries-to-mavencentral-2021/#continuous-integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,8 +89,19 @@ tasks.findByName("compileKotlinLinuxX64")?.mustRunAfter("cinteropInteropLinuxX64
 val os: DefaultOperatingSystem = DefaultNativePlatform.getCurrentOperatingSystem()
 tasks.matching { it.name.contains("linux", true) }.configureEach { onlyIf { os.isLinux } }
 
-// Split tests in CI.
+// Split compilation and test in CI.
 // Apple-specific targets on macOS, Windows-specific targets on Windows, everything else on Linux.
+tasks.register("ciCompile") {
+    group = "build"
+    when {
+        os.isLinux -> dependsOn(
+            tasks.matching { it.name.matches(Regex("^compileKotlin(?!Ios|Macos|Mingw).*$")) }.map { it.name })
+        os.isMacOsX -> dependsOn(
+            tasks.matching { it.name.matches(Regex("^compileKotlin(Ios|Macos).*$")) }.map { it.name })
+        os.isWindows -> dependsOn(
+            tasks.matching { it.name.matches(Regex("^compileKotlin(Mingw).*$")) }.map { it.name })
+    }
+}
 tasks.register("ciTests") {
     group = "verification"
     when {
