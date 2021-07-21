829c6c2eb1e69273e39e928a76e992b24af45cb5
Gonçalo Silva <goncalossilva@gmail.com>
2021-07-22 00:42:06 +0100

    Destructure matches more idiomatically

--- buildSrc/src/main/kotlin/cinterop.gradle.kts
+++ buildSrc/src/main/kotlin/cinterop.gradle.kts
@@ -3,9 +3,9 @@
  */
 
 tasks.configureEach {
-    val match = "cinterop.+([A-Z][a-z]+[A-Z][a-z]*\\d\\d.*)".toRegex().matchEntire(name)
+    val match = "cinterop.+([A-Z][a-z]+)([A-Z][a-z]*\\d\\d.*)".toRegex().matchEntire(name)
     if (match != null) {
-        val target = match.groupValues[1]
+        val (target) = match.destructured
         tasks.findByName("compileKotlin$target")?.mustRunAfter(tasks.getByName(match.value))
     }
 }
