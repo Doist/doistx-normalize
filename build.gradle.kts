1bc221a47c7800a39cd5cc3395fb27f028407996
Gonçalo Silva <goncalossilva@gmail.com>
2021-07-26 16:49:37 +0100

    Don't publish root target without all targets enabled
    Doing so would result in a build that is unaware of all of its
    targets. Disabled targets won't be consumable, even if published
    later on. The root publication must be aware of all of them, and
    for that, they need to be enabled.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,13 @@ repositories {
 kotlin {
     explicitApi()
 }
+
+// Sanity check before attempting to publish root target without having all targets enabled.
+tasks.named("publishKotlinMultiplatform") {
+    doFirst {
+        if (findProperty("targets") != "all") {
+            throw IllegalStateException(
+                "Configuration is set to publish root target without all targets enabled.")
+        }
+    }
+}
