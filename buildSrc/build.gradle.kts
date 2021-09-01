42bb6e620d5ffb046a31f3a044e0dfdba1fae2ec
Gonçalo Silva <goncalossilva@gmail.com>
2021-09-01 01:12:50 +0100

    Update Kotlin to 1.5.30

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
