82c528e0729b6b1f66738d04107adf7bf91c74e5
Gonçalo Silva <goncalossilva@gmail.com>
2024-07-16 19:10:22 +0100

    Update to Kotlin 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("multiplatform")
     id("targets")
     id("cinterop")
     id("publish")
-    id("com.goncalossilva.resources") version "0.4.0"
+    id("com.goncalossilva.resources") version "0.9.0"
 }
 
 repositories {
@@ -16,27 +15,22 @@ repositories {
 }
 
 kotlin {
-    jvmToolchain(11)
-
     explicitApi()
+
+    jvmToolchain(11)
 
     sourceSets {
         all {
             languageSettings.optIn("kotlinx.cinterop.ExperimentalForeignApi")
         }
 
-        @Suppress("UNUSED_VARIABLE")
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("com.goncalossilva:resources:0.4.2")
+                implementation("com.goncalossilva:resources:0.9.0")
             }
         }
     }
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    options.release.set(8)
 }
 
 rootProject.plugins.withType(YarnPlugin::class.java) {
